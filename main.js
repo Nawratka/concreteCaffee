cde837e10aef7ef9115c4aebf4d76a49af2f4bef
Kamila <kamilanawrat@gmail.com>
2024-09-30 19:25:05 +0200

    leaflet map error handle

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const circleText = document.querySelector('.circle .text');
+let root = document.documentElement;
 const burgerBtn = document.querySelector('#nav-burger');
 const headerNav = document.querySelector('.nav');
 const headerNavItems = document.querySelectorAll('#top-bar .main-nav-item');
 const miniLogo = document.querySelector('.logo-mini-link');
-let root = document.documentElement;
+const circleText = document.querySelector('.circle .text');
 
-// BURGER BTN ANIMATION HANDLE
+// ----- BURGER BTN ANIMATION HANDLE
 
 burgerBtn.addEventListener('click', (e) => {
 	if (e.target !== burgerBtn) return;
@@ -15,7 +15,7 @@ burgerBtn.addEventListener('click', (e) => {
 	headerNav.classList.toggle('open');
 });
 
-// CLOSING NAV MENU AFTER ITEM CHOICE / MINI LOGO CLICK
+// ----- CLOSING NAV MENU AFTER ITEM CHOICE / MINI LOGO CLICK
 
 headerNav.addEventListener('click', (e) => {
 	if (e.target.classList.contains('main-nav-item')) {
@@ -31,7 +31,7 @@ miniLogo.addEventListener('click', (e) => {
 	headerNav.classList.remove('open');
 });
 
-// TURNING TEXT IN MENU SECTION
+// ----- TURNING TEXT IN MENU SECTION
 circleText.innerHTML = circleText.innerText
 	.split('')
 	.map(
@@ -39,7 +39,7 @@ circleText.innerHTML = circleText.innerText
 	)
 	.join('');
 
-// CHANGE IMG POSITION DURING SCROLLING
+// ----- CHANGE IMG POSITION DURING SCROLLING
 const handleImgPosition = () => {
 	const scroll = window.scrollY;
 	const pageHeight = document.body.offsetHeight;
@@ -52,28 +52,47 @@ const handleImgPosition = () => {
 
 window.addEventListener('scroll', handleImgPosition);
 
-// LEAFLET MAP
-const map = L.map('map').setView([50.0849, 19.9718], 15);
 
-// STYLING MAP
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-	maxZoom: 19,
-	// attribution:
-	// 	'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a> & <a href="https://carto.com/">CartoDB</a>',
-}).addTo(map);
+// ----- LEAFLET MAP
+function initializeMap() {
+	try {
+		const map = L.map('map').setView([50.0849, 19.9718], 15);
 
-// CREATING CUSTOM ICON & SETTING POPUP
-const customIcon = L.icon({
-	iconUrl: './img/map-pin.svg',
-	iconSize: [48, 48],
-	iconAnchor: [24, 48],
-	popupAnchor: [0, -63],
-});
+		// STYLING MAP
+		L.tileLayer(
+			'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+			{
+				maxZoom: 19,
+				// attribution:
+				// 	'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a> & <a href="https://carto.com/">CartoDB</a>',
+			}
+		).addTo(map);
 
-// ADD ICON TO MAP
-const marker = L.marker([50.0849, 19.9718], { icon: customIcon }).addTo(map);
-marker
-	.bindPopup(
-		'<b>Hello! Tu jesteśmy</b><br>Concrete COFFEE & BAKERY<hr>Codziennie 6 - 17'
-	)
-	.openPopup();
+		// CREATING CUSTOM ICON & SETTING POPUP
+		const customIcon = L.icon({
+			iconUrl: './img/map-pin.svg',
+			iconSize: [48, 48],
+			iconAnchor: [24, 48],
+			popupAnchor: [0, -63],
+		});
+
+		// ADD ICON TO MAP
+		const marker = L.marker([50.0849, 19.9718], { icon: customIcon }).addTo(
+			map
+		);
+		marker
+			.bindPopup(
+				'<b>Hello! Tu jesteśmy</b><br>Concrete COFFEE & BAKERY<hr>Codziennie 6 - 17'
+			)
+			.openPopup();
+	} catch (error) {
+		console.log(
+			'🔴🔴🔴 Wystąpił błąd podczas inicjalizacji mapy: ',
+			error,
+			' 🔴🔴🔴'
+		);
+		alert('Nie udało się załadować mapy. Spróbuj ponownie później.');
+	}
+}
+
+initializeMap();
